07bc3d5ec3d21177f7fdd9d884ab9040714d0ce6
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-24 22:51:48 +0100

    Add(DiccionariController.java): createParaula method

--- back-end/diccionari/src/main/java/ifc33b/dwesc/diccionari/controller/DiccionariController.java
+++ back-end/diccionari/src/main/java/ifc33b/dwesc/diccionari/controller/DiccionariController.java
@@ -1,6 +1,7 @@
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ifc33b.dwesc.diccionari.dto.ParaulaRequest;
 import ifc33b.dwesc.diccionari.dto.ParaulaResponse;
 import ifc33b.dwesc.diccionari.service.ParaulaService;
 
@@ -29,8 +31,12 @@ public class DiccionariController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping()
-    public String postMethodName(@RequestBody String entity) {
-        return entity;
+    @PostMapping() // Crear una palabra
+    public ResponseEntity<ParaulaResponse> createParaula(@RequestBody ParaulaRequest request) {
+        // Service
+        ParaulaResponse response = paraulaService.createParaula(request);
+
+        // HTTP Response
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
