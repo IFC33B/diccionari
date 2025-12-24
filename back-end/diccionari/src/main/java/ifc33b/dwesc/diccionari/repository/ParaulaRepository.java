b19029bd694c66baa8a6dec38e8e71e2696c264e
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-24 22:20:41 +0100

    Create: ParaulaRepository.java

---
+++ back-end/diccionari/src/main/java/ifc33b/dwesc/diccionari/repository/ParaulaRepository.java
@@ -0,0 +1,9 @@
+package ifc33b.dwesc.diccionari.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ifc33b.dwesc.diccionari.model.Paraula;
+
+@Repository
+public interface ParaulaRepository extends JpaRepository<Paraula, Long> {}
