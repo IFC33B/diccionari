96bb55825ce7eb4b25147ffbb26690436e3747f6
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-24 22:19:11 +0100

    Add(Paraula.java): GenerationType import

--- back-end/diccionari/src/main/java/ifc33b/dwesc/diccionari/model/Paraula.java
+++ back-end/diccionari/src/main/java/ifc33b/dwesc/diccionari/model/Paraula.java
@@ -3,6 +3,7 @@ package ifc33b.dwesc.diccionari.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
