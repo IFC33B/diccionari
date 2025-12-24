5d569a40c37b4074e9ae231b4c96272e6bf04fb8
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-24 22:07:36 +0100

    Create: backend structure

---
+++ back-end/diccionari/src/main/java/ifc33b/dwesc/diccionari/DiccionariApplication.java
@@ -0,0 +1,13 @@
+package ifc33b.dwesc.diccionari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiccionariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiccionariApplication.class, args);
+	}
+
+}
---
+++ back-end/diccionari/src/test/java/ifc33b/dwesc/diccionari/DiccionariApplicationTests.java
@@ -0,0 +1,13 @@
+package ifc33b.dwesc.diccionari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiccionariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
