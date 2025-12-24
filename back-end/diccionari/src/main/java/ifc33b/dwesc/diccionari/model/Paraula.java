b96c434f8a1f9a4ce34e74353f90369fdd68dcf0
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-24 22:18:02 +0100

    Create: Paraula model

---
+++ back-end/diccionari/src/main/java/ifc33b/dwesc/diccionari/model/Paraula.java
@@ -0,0 +1,32 @@
+package ifc33b.dwesc.diccionari.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "paraula")
+public class Paraula {
+    // Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String terme;
+
+    @Column(nullable = false)
+    private String definicio;
+
+    // Constructor
+    public Paraula() {}
+
+    public Paraula(String terme, String definicio) {
+        this.setTerme(terme);
+        this.setDefinicio(definicio);
+    }
+}
