84c8342578f5f702271909130c85eb3c5783c037
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-25 15:24:01 +0100

    Add(Paraula.service): handleError method

--- front-end/diccionari/src/app/services/paraula.service.ts
+++ front-end/diccionari/src/app/services/paraula.service.ts
@@ -1,8 +1,29 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Paraula } from '../models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ParaulaService {
-  
+  private apiURL = 'http://localhost:8080/api/diccionari'
+
+  constructor(private http: HttpClient) {};
+
+  // Gestión de errores
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Error desconocido';
+
+    if (error.error instanceof ErrorEvent) {
+      // Error del cliente
+      errorMessage = `Error: ${error.error.message}`
+    } else {
+      // Error del servidor
+      errorMessage = `Codigo de error: ${error.status}, Mensaje: ${error.message}`;
+    }
+
+    console.log(errorMessage);
+    return throwError(() => Error(errorMessage))
+  }
 }
